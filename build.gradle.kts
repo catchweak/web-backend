014ddcaf1e6d593f4785617b30b5ba68a17930a3
ydj515 <ydj515@hanmail.net>
2024-06-29 13:54:16 +0900

    refactor: gradle 에서 lombok 제거 및 application.yml secret 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,6 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	implementation("org.projectlombok:lombok")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 
 	/* jpa */
