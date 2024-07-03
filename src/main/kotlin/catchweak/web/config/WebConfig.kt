f16d3b3d4b8159dea3744dbd4613b7644a7ee255
ydj515 <ydj515@hanmail.net>
2024-07-04 00:38:54 +0900

    feat: sereialize를 위한 objectmapper bean으로 추가

--- src/main/kotlin/catchweak/web/config/WebConfig.kt
+++ src/main/kotlin/catchweak/web/config/WebConfig.kt
@@ -1,9 +1,18 @@
 package catchweak.web.config
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.time.LocalDateTime
 
 @Configuration
 class WebConfig {
@@ -20,5 +29,28 @@ class WebConfig {
                     .allowedOrigins("http://localhost:3000")
             }
         }
+    }
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+
+        val objectMapper = ObjectMapper()
+        val javaTimeModule = JavaTimeModule()
+
+        objectMapper.registerModule(javaTimeModule)
+
+        objectMapper.registerModule(
+            KotlinModule.Builder()
+                .withReflectionCacheSize(512)
+                .configure(KotlinFeature.NullToEmptyCollection, true)
+                .configure(KotlinFeature.NullToEmptyMap, true)
+                .configure(KotlinFeature.NullIsSameAsDefault, true)
+                .configure(KotlinFeature.SingletonSupport, false)
+                .configure(KotlinFeature.StrictNullChecks, false)
+                .build())
+
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) // 시간은 문자열 포맷팅
+
+        return objectMapper
     }
 }
