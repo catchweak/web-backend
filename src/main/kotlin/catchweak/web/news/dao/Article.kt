3449fb512027a4ec973b6cf53e6cb25879e1976b
ydj515 <ydj515@hanmail.net>
2024-07-21 17:16:41 +0900

    refactor: article과 category를 manytomany로 변경하면서 코드 변경

--- src/main/kotlin/catchweak/web/news/dao/Article.kt
+++ src/main/kotlin/catchweak/web/news/dao/Article.kt
@@ -1,5 +1,6 @@
 package catchweak.web.news.dao
 
+import com.fasterxml.jackson.annotation.JsonManagedReference
 import jakarta.persistence.*
 
 @Entity
@@ -9,9 +10,14 @@ data class Article(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
 
-    @ManyToOne
-    @JoinColumn(name = "category_id")
-    val category: Category? = null,
+    @ManyToMany
+    @JoinTable(
+        name = "article_category",
+        joinColumns = [JoinColumn(name = "article_id")],
+        inverseJoinColumns = [JoinColumn(name = "category_id")]
+    )
+    @JsonManagedReference
+    val categories: Set<Category> = emptySet(),
 
     val url: String? = null,
     val originUrl: String? = null,
@@ -25,4 +31,17 @@ data class Article(
 
     @Column(name = "collected_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     val collectedAt: java.sql.Timestamp? = java.sql.Timestamp(System.currentTimeMillis())
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Article
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
--- src/main/kotlin/catchweak/web/news/dao/es/ArticleDocument.kt
+++ src/main/kotlin/catchweak/web/news/dao/es/ArticleDocument.kt
@@ -1,26 +1,19 @@
 package catchweak.web.news.dao.es
 
-import org.springframework.data.annotation.Id
 import org.springframework.data.elasticsearch.annotations.Document
 
 @Document(indexName = "articles")
 data class ArticleDocument(
-    @Id
     val id: Long,
-    val categoryId: Long?,
-    val categoryName: String?,
-    val categoryCode: String?,
-    val categoryParentCode: String?,
-    val siteId: Long?,
-    val siteName: String?,
-    val url: String?,
-    val originUrl: String?,
-    val headline: String?,
-    val body: String?,
-    val imgUrl: String?,
-    val summary: String?,
-    val author: String?,
-    val articleCreatedAt: String?,
-    val articleUpdatedAt: String?,
-    val collectedAt: String?,
+    val categories: List<CategoryDocument>? = null,
+    val url: String? = null,
+    val originUrl: String? = null,
+    val headline: String? = null,
+    val body: String? = null,
+    val imgUrl: String? = null,
+    val summary: String? = null,
+    val author: String? = null,
+    val articleCreatedAt: String? = null,
+    val articleUpdatedAt: String? = null,
+    val collectedAt: String? = null
 )
