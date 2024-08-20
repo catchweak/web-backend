78043769fcf4e3b150ffcc740678337776449a97
이태윤 <tyleeg0901@gmail.com>
2024-08-20 19:05:29 +0900

    update: Api Request 규격 관련 누락 소스 추가

--- src/main/kotlin/catchweak/web/common/payload/request/ApiRequest.kt
+++ src/main/kotlin/catchweak/web/common/payload/request/ApiRequest.kt
@@ -1,4 +1,16 @@
 package catchweak.web.common.payload.request
 
-class ApiRequest {
+import java.time.LocalDateTime
+import java.util.*
+
+abstract class ApiRequest {
+    // 고유 요청 ID값
+    val requestId: String = UUID.randomUUID().toString()
+
+    // 요청 시간
+    val timestamp: LocalDateTime = LocalDateTime.now()
+
+    open fun validate() {}
+
+    abstract fun specificValidate()
 }
--- src/main/kotlin/catchweak/web/member/repository/MemberRepository.kt
+++ src/main/kotlin/catchweak/web/member/repository/MemberRepository.kt
@@ -1,8 +1,11 @@
 package catchweak.web.member.repository
 
 import catchweak.web.member.model.entity.Member
+import java.util.*
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MemberRepository : JpaRepository<Member, Long> {
     fun existsByUserId(userId: String): Boolean
+
+    fun findByUserId(userId: String): Optional<Member>
 }
